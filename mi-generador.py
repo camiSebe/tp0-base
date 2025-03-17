c530d239dcdc0c5021d9f7b7a6b4808e91e062ec
csebellin@fi.uba.ar <csebellin@fi.uba.ar>
2025-03-17 16:11:35 -0300

    Fix: Networks ya no se crea dentro de services y no se ordenan las claves en el yaml

--- mi-generador.py
+++ mi-generador.py
@@ -3,7 +3,6 @@ import yaml
 
 def generar_docker_compose(archivo_salida, cant_clientes):
     docker_compose_data = {
-        "name": "tp0",
         "services": {
             "server": {
                 "container_name": "server",
@@ -14,15 +13,15 @@ def generar_docker_compose(archivo_salida, cant_clientes):
                     "LOGGING_LEVEL=DEBUG",
                 ],
                 "networks": ["testing_net"],
-            },
-            "networks": {
-                "testing_net": {
-                    "ipam": {
-                        "driver": "default",
-                        "config": [
-                            {"subnet": "172.25.125.0/24"}
-                        ]
-                    }
+            }
+        },
+        "networks": {
+            "testing_net": {
+                "ipam": {
+                    "driver": "default",
+                    "config": [
+                        {"subnet": "172.25.125.0/24"}
+                    ]
                 }
             }
         }
@@ -42,7 +41,7 @@ def generar_docker_compose(archivo_salida, cant_clientes):
         }
     
     with open(archivo_salida, "w") as file:
-        yaml.dump(docker_compose_data, file)
+        yaml.dump(docker_compose_data, file, sort_keys=False)
 
 
 
