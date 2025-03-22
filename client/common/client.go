bef3270be21c3b3cbfa9e7be37562e4bc36c1b87
csebellin@fi.uba.ar <csebellin@fi.uba.ar>
2025-03-22 14:38:27 -0300

    FIx: Se elimina el uso de exit cuando se usa el signal channel

--- client/common/client.go
+++ client/common/client.go
@@ -2,10 +2,6 @@ package common
 
 import (
 	"net"
-	"os"
-	"os/signal"
-	"sync"
-	"syscall"
 	"time"
 
 	"github.com/op/go-logging"
@@ -25,8 +21,6 @@ type ClientConfig struct {
 type Client struct {
 	config ClientConfig
 	conn   net.Conn
-	done  chan struct{}
-	wg   sync.WaitGroup
 }
 
 // NewClient Initializes a new client receiving the configuration
@@ -34,7 +28,6 @@ type Client struct {
 func NewClient(config ClientConfig) *Client {
 	client := &Client{
 		config: config,
-		done:  make(chan struct{}),
 	}
 	return client
 }
@@ -58,15 +51,6 @@ func (c *Client) createClientSocket() error {
 
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-
-	go func() {
-		<-sigChan
-		log.Infof("action: stop | result: success | client_id: %v", c.config.ID)
-		c.StopClient()
-	}()
-
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
@@ -93,15 +77,8 @@ func (c *Client) StartClientLoop() {
 }
 
 func (c *Client) StopClient() {
-	select {
-	case <-c.done:
-		// Already closed, nothing to do
-		return
-	default:
-		close(c.done)
-		if c.conn != nil {
-			c.conn.Close()
-			log.Infof("action: stop_client | result: success | client_id: %v", c.config.ID)
-		}
+	if c.conn != nil {
+		c.conn.Close()
+		log.Infof("action: stop_client | result: success | client_id: %v", c.config.ID)
 	}
 }
